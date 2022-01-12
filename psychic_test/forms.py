3e60db9a5d6bf7938c44eb798e95097e7f34fe19
Roman <slyusarrv@yandex.ru>
2022-01-12 09:49:08 +0500

    added message and form number validation

--- psychic_test/forms.py
+++ psychic_test/forms.py
@@ -12,3 +12,11 @@ class QuestionForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
+
+    def clean_number(self):
+        number = self.cleaned_data['number']
+        if number < 10 or number > 99:
+            raise forms.ValidationError(
+                'Неправильный ввод. Необходимо загадать двузначное число.'
+            )
+        return number
